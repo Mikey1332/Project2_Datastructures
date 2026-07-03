453465ab79bdc1e7810fc20185de9d187e3eabb2
kaidenkurian <kaidenkurian@gmail.com>
2026-07-03 16:30:52 -0400

    renamed function

--- src/trie.h
+++ src/trie.h
@@ -26,7 +26,7 @@ public:
     void insert(string name, char sex, int year, int count);
     bool search(string name, char sex, int year); // searching to see if a specific name exists, with a corresponding year and sex
     void print(int year); // just a basic print function for maybe all names + gender in a year with their count.
-    int getAllTimeTotal(string name, char sex);
+    int getAllTimeTotal(string name, char sex, bool pref);
     int getYearTotal(string name, char sex, int year);
 
     // needed a destructor
